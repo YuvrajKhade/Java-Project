71bb282abcc6c8a499e99a5449d9979aacb65251
YuvrajKhade <khadeyuvraj8@gmail.com>
2024-03-09 01:41:37 +0530

    database  connectivity

--- src/main/java/Training_placement/StudentAddPage.java
+++ src/main/java/Training_placement/StudentAddPage.java
@@ -4,6 +4,9 @@
  */
 package Training_placement;
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -16,7 +19,27 @@ public class StudentAddPage extends javax.swing.JFrame {
      */
     public StudentAddPage() {
         initComponents();
+        connect();
     }
+   static Connection conn;
+    static PreparedStatement stmt;
+    
+    
+    public static void connect() 
+    {
+             String  dburl="jdbc:oracle:thin:@//localhost:1521/xe";
+             String user="system";
+             String pass="system";
+                 try {
+                     //Class.forName("oracle.jdbc.OracleDiver");
+                     conn=DriverManager.getConnection(dburl,user,pass);
+                 } catch (SQLException ex) {
+                     Logger.getLogger(StudentAddPage.class.getName()).log(Level.SEVERE, null, ex);
+                 }
+        
+        
+    }
+    
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -27,23 +50,23 @@ public class StudentAddPage extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        buttonGroup1 = new javax.swing.ButtonGroup();
+        gendergrp = new javax.swing.ButtonGroup();
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        cmpid = new javax.swing.JTextField();
+        stuname = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
-        cmpid1 = new javax.swing.JTextField();
+        stuprn = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
-        cmpid2 = new javax.swing.JTextField();
+        stugmail = new javax.swing.JTextField();
         jLabel5 = new javax.swing.JLabel();
-        cmpid3 = new javax.swing.JTextField();
+        stucont = new javax.swing.JTextField();
         jLabel6 = new javax.swing.JLabel();
-        cmpid4 = new javax.swing.JTextField();
+        stucgpa = new javax.swing.JTextField();
         jLabel8 = new javax.swing.JLabel();
         jLabel9 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTextArea1 = new javax.swing.JTextArea();
+        stuskills = new javax.swing.JTextArea();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
@@ -89,9 +112,9 @@ public class StudentAddPage extends javax.swing.JFrame {
         jLabel9.setForeground(new java.awt.Color(204, 51, 0));
         jLabel9.setText("Student skills:");
 
-        jTextArea1.setColumns(20);
-        jTextArea1.setRows(5);
-        jScrollPane1.setViewportView(jTextArea1);
+        stuskills.setColumns(20);
+        stuskills.setRows(5);
+        jScrollPane1.setViewportView(stuskills);
 
         jButton1.setBackground(new java.awt.Color(153, 51, 0));
         jButton1.setFont(new java.awt.Font("Segoe UI Black", 1, 18)); // NOI18N
@@ -125,7 +148,7 @@ public class StudentAddPage extends javax.swing.JFrame {
             }
         });
 
-        buttonGroup1.add(male);
+        gendergrp.add(male);
         male.setText("Male");
         male.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -133,7 +156,7 @@ public class StudentAddPage extends javax.swing.JFrame {
             }
         });
 
-        buttonGroup1.add(female);
+        gendergrp.add(female);
         female.setText("Female");
         female.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -158,13 +181,13 @@ public class StudentAddPage extends javax.swing.JFrame {
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addComponent(jLabel2)
-                                    .addComponent(cmpid, javax.swing.GroupLayout.PREFERRED_SIZE, 207, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(stuname, javax.swing.GroupLayout.PREFERRED_SIZE, 207, javax.swing.GroupLayout.PREFERRED_SIZE)
                                     .addComponent(jLabel4)
-                                    .addComponent(cmpid2, javax.swing.GroupLayout.PREFERRED_SIZE, 271, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                    .addComponent(stugmail, javax.swing.GroupLayout.PREFERRED_SIZE, 271, javax.swing.GroupLayout.PREFERRED_SIZE))
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 65, Short.MAX_VALUE))
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(cmpid1, javax.swing.GroupLayout.PREFERRED_SIZE, 183, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(stuprn, javax.swing.GroupLayout.PREFERRED_SIZE, 183, javax.swing.GroupLayout.PREFERRED_SIZE)
                                     .addComponent(jLabel3))
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                 .addComponent(jLabel10, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -190,9 +213,9 @@ public class StudentAddPage extends javax.swing.JFrame {
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addComponent(jLabel5)
-                                    .addComponent(cmpid3, javax.swing.GroupLayout.PREFERRED_SIZE, 235, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(stucont, javax.swing.GroupLayout.PREFERRED_SIZE, 235, javax.swing.GroupLayout.PREFERRED_SIZE)
                                     .addComponent(jLabel6)
-                                    .addComponent(cmpid4, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                    .addComponent(stucgpa, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE))
                                 .addGap(0, 0, Short.MAX_VALUE))
                             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                                 .addGap(0, 0, Short.MAX_VALUE)
@@ -223,7 +246,7 @@ public class StudentAddPage extends javax.swing.JFrame {
                                 .addGap(41, 41, 41)
                                 .addComponent(jLabel3)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(cmpid1, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addComponent(stuprn, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addGap(33, 33, 33)
                                 .addComponent(jLabel8)
@@ -241,22 +264,22 @@ public class StudentAddPage extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(cmpid, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(stuname, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(jLabel4)
                         .addGap(11, 11, 11)
-                        .addComponent(cmpid2, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(stugmail, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(jLabel5)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(cmpid3, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(stucont, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
                         .addComponent(jLabel6)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(cmpid4, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(stucgpa, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 12, Short.MAX_VALUE)
                         .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addComponent(jLabel7, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
@@ -302,18 +325,54 @@ public class StudentAddPage extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-             String  dburl="jdbc:oracle:thin@localhost:1525:xe";
-             String user="localhost";
-             String pass="system";
-             try
-             {
-                 Connection conn=DriverManager.getConnection(dburl,user,pass);
-                 System.out.println("Database is connected");
-             }
-             catch(SQLException e)
-             {
-                 System.out.println("Oracle error"+e);
-             }
+             
+        String prn=stuprn.getText();
+        String name=stuname.getText();
+        String gmail=stugmail.getText();
+        String con=stucont.getText();
+        String cgpa=stucgpa.getText();
+        String gen="";
+        if(male.isSelected())
+        {
+            gen=male.getText();
+        }
+        else if(female.isSelected())
+        {
+            gen=female.getText();
+        }
+        String skills=stuskills.getText();
+        
+        try {
+            stmt=conn.prepareStatement("insert into tandpstudentdata(prn_no,stu_name,email_id,contact,gender,grade,skills)values(?,?,?,?,?,?,?)");
+            stmt.setString(1,prn);
+            stmt.setString(2,name);
+            stmt.setString(3,gmail);
+            stmt.setString(4,con);
+            stmt.setString(5,cgpa);
+            stmt.setString(6,gen);
+            stmt.setString(7,skills);
+            int k=stmt.executeUpdate();
+            if(k==1)
+            {
+                JOptionPane.showMessageDialog(this,"Data added successfully..!!"); 
+                stuprn.setText("");
+                stuname.setText("");
+                stugmail.setText("");
+                stucont.setText("");
+                stucgpa.setText("");
+                stuskills.setText("");
+                gendergrp.clearSelection();
+                stuname.requestFocus();
+            }
+            else
+            {
+                JOptionPane.showMessageDialog(this,"Unable to load"); 
+            }
+            
+          
+        } catch (SQLException ex) {
+            Logger.getLogger(StudentAddPage.class.getName()).log(Level.SEVERE, null, ex);
+        }
                
     }//GEN-LAST:event_jButton2ActionPerformed
 
@@ -353,13 +412,8 @@ public class StudentAddPage extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.ButtonGroup buttonGroup1;
-    private javax.swing.JTextField cmpid;
-    private javax.swing.JTextField cmpid1;
-    private javax.swing.JTextField cmpid2;
-    private javax.swing.JTextField cmpid3;
-    private javax.swing.JTextField cmpid4;
     private javax.swing.JRadioButton female;
+    private javax.swing.ButtonGroup gendergrp;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
@@ -375,7 +429,12 @@ public class StudentAddPage extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextArea jTextArea1;
     private javax.swing.JRadioButton male;
+    private javax.swing.JTextField stucgpa;
+    private javax.swing.JTextField stucont;
+    private javax.swing.JTextField stugmail;
+    private javax.swing.JTextField stuname;
+    private javax.swing.JTextField stuprn;
+    private javax.swing.JTextArea stuskills;
     // End of variables declaration//GEN-END:variables
 }
